7026ac8b85319fdfe1cf9461c9d654470f324056
kkuramitsu <kkuramitsu@gmail.com>
2023-03-04 04:13:00 +0900

    0.0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def _requires_from_file(filename):
 
 
 setup(name="trainmaru",
-      version="0.0.1",
+      version="0.0.2",
       license='MIT',
       author='Kimio Kuramitsu',
       description="Deep Learning",
