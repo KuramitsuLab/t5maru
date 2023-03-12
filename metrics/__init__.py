6c1b71e2d7183e29e8a95adfc6013dbced5c19e9
kkuramitsu <kkuramitsu@gmail.com>
2023-03-13 00:58:53 +0900

    code score

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,5 +1,5 @@
-from .score_py import eval_dump_py, get_filename
+from .score_py import score_py, get_filename
 
 
 def dump_score(refs: list, preds: list, outputfile, lang='py', testfile='', model_id=''):
-    eval_dump_py(refs, preds, outputfile, testfile, model_id)
+    score_py(refs, preds, outputfile, testfile, model_id)
